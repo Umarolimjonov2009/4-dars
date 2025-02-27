c599b67b578162ec8a79e689d5016bff4aec9536
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-02-27 18:01:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const ism=prompt("ism kiriting!");
+const harf=prompt("harf kiriting!");
+
+
+if(ism. toLowerCase().includes(harf.toLowerCase())){
+    alert(ism+ "ismida  "   + harf +  "harfi mavjud")
+}else{
+    alert(ism+ "ismida  "   + harf +   "harfi mavjud emas")
+}
